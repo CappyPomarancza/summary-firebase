ff500a8bd465e2a83b43334a0c9e70db60f0a6cf
Kacper Swietlaga <kacperres@gmail.com>
2018-08-10 15:06:21 +0200

    added counter conected fith firebase

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,78 @@
 import React from 'react'
 
 class Counter extends React.Component {
-    
     state = {
         number: 0
     }
 
+    componentDidMount() {
+        fetch('https://todo-e8a15.firebaseio.com/cappy-counter/number/.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    number: data
+                })
+            })
+    }
+    
+    updataCounter = () => {
+
+        const request = {
+            method: 'PUT',
+            body: JSON.stringify(
+               this.state.number
+            )
+        }
+
+        fetch('https://todo-e8a15.firebaseio.com/cappy-counter/number/.json'
+            , request)
+    }
+   
+    incHandler = () => {
+
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+    decHandler = () => {
+        this.setState({
+            number: this.state.number - 1
+        })
+
+    }
+    reset = () => {
+        this.setState({
+            number: this.state.number = 0
+        })
+
+    }
+
+
 
     render(){
-        return (
-            <h2>Hello in my JFDDL5 Counter</h2>
 
+       this.updataCounter()
+        
+        return (
+            <div>
+                <h2>Hello in my JFDDL5 Counter</h2>
+                <h1>{this.state.number}</h1>
+                <button
+                    onClick={this.incHandler}
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.decHandler}
+                >
+                    -
+                </button>
+                <button
+                    onClick={this.reset}
+                >
+                    RESET
+                </button>
+            </div >
 
 
         )
---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyAmTXsrM_GQyySgxE9qfhdqmcgx3XF_TzY",
+    authDomain: "survey-app-84f53.firebaseapp.com",
+    databaseURL: "https://survey-app-84f53.firebaseio.com",
+    projectId: "survey-app-84f53",
+    storageBucket: "survey-app-84f53.appspot.com",
+    messagingSenderId: "635698436786"
+  }
+  firebase.initializeApp(config)
+
+  export const database = firebase.database()
+  export const auth = firebase.auth()
+  export const googleProvider=  new firebase.auth.GoogleAuthProvider()
