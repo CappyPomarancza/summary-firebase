8a11eb6cd12f9cd59552e39e6ddec23e17d82915
Kacper Swietlaga <kacperres@gmail.com>
2018-08-10 13:02:44 +0200

    added empty Counter commponent

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    
+    state = {
+        number: 0
+    }
+
+
+    render(){
+        return (
+            <h2>Hello in my JFDDL5 Counter</h2>
+
+
+
+        )
+    }
+}
+export default Counter
